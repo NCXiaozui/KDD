1e13127b605a63098e0ff144cebcb900733a23dc
xylander <xy@Majesty.(none)>
2017-03-26 23:30:37 +0800

    pick up some features for prediction

--- KDD/Exchange_form.py
+++ KDD/Exchange_form.py
@@ -11,6 +11,7 @@ def exchange_formation(file_in,file_out_feature,file_out_target):
     reader = csv.reader(csvfile)
     feature_txt = open(file_out_feature,'a+')
     target_txt = open(file_out_target,'a+')
+    fina_txt = open('/home/xy/Data/KDD/final.txt','a+')
     flag = 0
     for line in reader:
         if flag == 0:
@@ -19,9 +20,11 @@ def exchange_formation(file_in,file_out_feature,file_out_target):
         else:
             for i in range(0,len(line)):
                 #print i
+
                 if i < len(line) - 1:
                     if i == 0 :
                         feature_txt.write('%d ' % (float(line[i][-2:]))) #记录日期
+                        fina_txt.write('%s ' % (line[i]))
                         continue
                     if i == 2 :
                         target_txt.write('%.5f\n'%(float(line[i])))
@@ -29,17 +32,20 @@ def exchange_formation(file_in,file_out_feature,file_out_target):
                     if i == 1 :
                         print line[i],line[i][0:2],line[i][3:5] #我们将小时和分钟分开来记录
                         feature_txt.write('%d %d '%(float(line[i][0:2]),float(line[i][3:5])))
-                    else:
+                    if i == 6 or i == 7 or i == 48 :
                         feature_txt.write('%.2f '%(float(line[i])))
+                    fina_txt.write('%s ' % (line[i]))
+
 
                 else:
                     feature_txt.write('%.2f\n'%(float(line[i])))
+                    fina_txt.write('%s\n' % (line[i]))
 
-
+    feature_txt.close()
     target_txt.close()
 
 if __name__ == '__main__':
-    file_in = '/home/xy/Data/KDD/Av_C_3_feature.csv'
-    file_out_feature = '/home/xy/Data/KDD/feature.txt'
-    file_out_target = '/home/xy/Data/KDD/target.txt'
+    file_in = '/home/xy/Data/KDD/predict_data/C_3_pred.csv'
+    file_out_feature = '/home/xy/Data/KDD/pred_feature.txt'
+    file_out_target = '/home/xy/Data/KDD/pred_target.txt'
     exchange_formation(file_in,file_out_feature,file_out_target)
--- KDD/GBDT_Model.py
+++ KDD/GBDT_Model.py
@@ -9,15 +9,16 @@ __author__ = 'XYlander'
 import numpy as np
 from sklearn.ensemble import GradientBoostingRegressor
 
+
 def Set_GBDT():  #Set GBDT model parameter
     gbdt = GradientBoostingRegressor(
-        loss='huber'     #loss function
+        loss='huber'     #loss function hunber / ls
         , learning_rate=0.1
         , n_estimators=1000
         , subsample=0.8
         , min_samples_split=2
         , min_samples_leaf=1
-        , max_depth=3
+        , max_depth=6
         , init=None
         , random_state=None
         , max_features=None
@@ -37,12 +38,42 @@ def train_model(file_feature,file_target,GBDT):
 
 
 
-def predict():
-    pass
+
+def predict(file_predict,GBDT,exam):
+    file_predict = np.genfromtxt(file_predict, dtype=np.float32)
+    exam = np.genfromtxt(exam, dtype=np.float32)
+    pred = GBDT.predict(file_predict)
+    total_err = 0
+    for i in range(pred.shape[0]):
+        print pred[i], exam[i]
+        err = abs(pred[i] - exam[i]) / exam[i]
+        total_err = total_err + err
+    print total_err / pred.shape[0]
+
+    return pred
+
+def output(final,pred):
+    final_file = open(final,'r')
+    result_file = open('/home/xy/Data/KDD/final_result.txt','w')
+    i = 0
+    for line in final_file:
+        line = line.strip('\n').split(' ')
+        #print line
+        line.append(str(pred[i]))
+        i += 1
+        result_file.write('%s\n'%(','.join(line)))
+    result_file.close()
+    final_file.close()
+
+
 
 if __name__ == '__main__':
     file_feature = '/home/xy/Data/KDD/feature.txt'
     file_target = '/home/xy/Data/KDD/target.txt'
+    file_predict = '/home/xy/Data/KDD/pred_feature.txt'
+    exam = '/home/xy/Data/KDD/pred_target.txt'
+    final = '/home/xy/Data/KDD/final.txt'
     GBDT = Set_GBDT()
     train_model(file_feature,file_target,GBDT)
-    predict()
+    pred = predict(file_predict,GBDT,exam)
+    output(final,pred)
